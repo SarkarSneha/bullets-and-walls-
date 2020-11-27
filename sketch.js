64be1e44e271bca148f83aa9cfffa1f712b30a92
SarkarSneha <73243873+SarkarSneha@users.noreply.github.com>
2020-11-27 21:30:13 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,54 +1,73 @@
 //assigning variables
-var car,wall
-var speed,weight
+var bullet,wall,thickness;
+var speed,weight;
+var lbullet,lwall
 
  function setup() {
     createCanvas(1600,400);
     
-    //giving random values to speed and weight
-    speed=random(55,90)
-    weight=random(400,1500)
+    //giving random values to speed , weight and thickness
+    speed=random(223,321)
+    weight=random(30,52)
+    thickness=random(22,83)
     
-    //creating car and wall sprites
-    car=createSprite(50, 200, 50, 50);
-   
-
-    wall=createSprite(1500,200,60,height/2)
+    //creating bullet and wall sprites
+    bullet=createSprite(50, 200, 59, 10);
+    bullet.velocityX=speed;
+    wall=createSprite(1200,200,thickness,height/2)
 
     wall.shapeColor=color(80,80,80)
-    car.shapeColor=color(225,225,225)
+    bullet.shapeColor=("white")
 
- 
-
-
+    
 }
 
-function draw() {
-    background(0);
-//the change
- car.velocityX=speed;
-    //changing colors according to the deformation rate 
-    if(wall.x-car.x<(car.width+wall.width)/2)
-     {
-      //car should stop after colliding
-      car.velocityX=0;
-      //formula of deformation
-      var deformation=0.5*weight*speed*speed/22509;
-      //if the deformation is greater than 180 considering it lethal as red
-      if(deformation>180)
-      {
-        car.shapeColor=("red")
-      }
-      //if the deformation is between 100 and 180 considering it average as yellow
-      if(deformation<180 && deformation>100)
-      {
-        car.shapeColor=("yellow")
-      }
-      //if the deformation is lesser than 100 considering it good as green
-      if(deformation<100)
-      {
-        car.shapeColor=("green")
-      }
-      }  
+function draw() { 
+  background("black");
+  
+ if( hasCollided(bullet,wall)){
+  bullet.velocityX=0;
+  var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+  //if the damage is greater than 10 change to red
+  if(damage>10)
+  {
+   wall.shapeColor=("red")
+  }
+  //if the damage is lesser than 10 change to green
+   if(damage<10)
+  {
+    wall.shapeColor=("green")
+  }
+ }
+
+ 
   drawSprites();
-      }
+    
+
+    
+}
+
+ 
+  //function hasCollided (lbullet,lwall)
+ // {
+    
+  
+  //bullet should stop after colliding
+  
+  //formula of damage
+  
+ 
+ // }  
+  
+    
+    
+    
+function hasCollided (lbullet,lwall)
+ { bulletRightEdge=lbullet.x + lbullet.width;
+    wallLeftEdge=lwall.x;
+    if(bulletRightEdge>=wallLeftEdge)
+    {
+      return true
+    }
+    return false
+  }
